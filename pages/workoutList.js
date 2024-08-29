6a0cf75e3f939a2f93bae4ebf8cd520b370d43ec
Sercan Sebil <sercansebil95@googlemail.com>
2024-08-29 15:26:11 +0200

    add Set to reduce muscle groups

---
+++ pages/workoutList.js
@@ -0,0 +1,62 @@
+import { exercises } from "@/lib/exercises";
+import { workouts } from "@/lib/workouts";
+import styled from "styled-components";
+
+const StickyHeader = styled.h1`
+  position: sticky;
+  top: 10px;
+`;
+
+export default function WorkoutsList() {
+  return (
+    <>
+      <header>
+        <StickyHeader>Choose Today's Workout</StickyHeader>
+      </header>
+      <main>
+        {workouts.map((workout) => {
+          const muscleGroupsSet = new Set();
+
+          workout.exercises.forEach((exercise) => {
+            const foundExercise = exercises.find(
+              (searchingExercise) =>
+                searchingExercise.id === exercise.exerciseId
+            );
+
+            if (foundExercise) {
+              foundExercise.muscleGroups.forEach((muscleGroup) => {
+                muscleGroupsSet.add(muscleGroup);
+              });
+            }
+          });
+
+          const reducedMuscleGroups = Array.from(muscleGroupsSet);
+
+          return (
+            <div key={workout.id}>
+              <h2>{workout.name}</h2>
+              <h3>Muscles In The Spotlight:</h3>
+              <p>{reducedMuscleGroups.join(", ")}</p>
+              <ul>
+                {workout.exercises.map((exercise) => {
+                  const foundExercise = exercises.find(
+                    (searchingExercise) =>
+                      searchingExercise.id === exercise.exerciseId
+                  );
+
+                  return (
+                    <li key={exercise.exerciseId}>
+                      <h3>{foundExercise.name}</h3>
+                      <p>Sets: {exercise.sets}</p>
+                      <p>Reps: {exercise.reps}</p>
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+          );
+        })}
+      </main>
+    </>
+  );
+}
