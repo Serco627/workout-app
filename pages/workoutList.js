5a352281aa0b4de5101e47d9414b8dcb79cf449d
Sercan Sebil <sercansebil95@googlemail.com>
2024-08-29 16:00:56 +0200

    add toggle functionality

--- pages/workoutList.js
+++ pages/workoutList.js
@@ -1,21 +1,26 @@
+import { useState } from "react";
 import { exercises } from "@/lib/exercises";
 import { workouts } from "@/lib/workouts";
-import styled from "styled-components";
-
-const StickyHeader = styled.h1`
-  position: sticky;
-  top: 10px;
-`;
 
 function findExerciseById(exerciseId) {
   return exercises.find((exercise) => exercise.id === exerciseId);
 }
 
 export default function WorkoutsList() {
+  const [showDetails, setShowDetails] = useState({});
+
+  // Toggle function to show or hide details for a specific workout
+  const toggleDetails = (workoutId) => {
+    setShowDetails((prev) => ({
+      ...prev,
+      [workoutId]: !prev[workoutId],
+    }));
+  };
+
   return (
     <>
       <header>
-        <StickyHeader>Choose Today's Workout</StickyHeader>
+        <h1>Choose Today's Workout</h1>
       </header>
       <main>
         {workouts.map((workout) => {
@@ -32,25 +37,31 @@ export default function WorkoutsList() {
           });
 
           const reducedMuscleGroups = Array.from(muscleGroupsSet);
+          const isDetailsVisible = showDetails[workout.id];
 
           return (
             <div key={workout.id}>
               <h2>{workout.name}</h2>
               <h3>Muscles In The Spotlight:</h3>
               <p>{reducedMuscleGroups.join(", ")}</p>
-              <ul>
-                {workout.exercises.map((exercise) => {
-                  const foundExercise = findExerciseById(exercise.exerciseId);
+              <button onClick={() => toggleDetails(workout.id)}>
+                {isDetailsVisible ? "Hide Details" : "Show Details"}
+              </button>
+              {isDetailsVisible && (
+                <ul>
+                  {workout.exercises.map((exercise) => {
+                    const foundExercise = findExerciseById(exercise.exerciseId);
 
-                  return (
-                    <li key={exercise.exerciseId}>
-                      <h3>{foundExercise.name}</h3>
-                      <p>Sets: {exercise.sets}</p>
-                      <p>Reps: {exercise.reps}</p>
-                    </li>
-                  );
-                })}
-              </ul>
+                    return (
+                      <li key={exercise.exerciseId}>
+                        <h3>{foundExercise.name}</h3>
+                        <p>Sets: {exercise.sets}</p>
+                        <p>Reps: {exercise.reps}</p>
+                      </li>
+                    );
+                  })}
+                </ul>
+              )}
             </div>
           );
         })}
