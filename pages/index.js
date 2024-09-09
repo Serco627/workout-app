cb8ae54b9a02858450f563b61a911f7bca2190c8
Sercan Sebil <sercansebil95@googlemail.com>
2024-09-09 15:56:49 +0200

    refactored styled components and exercise component

--- pages/index.js
+++ pages/index.js
@@ -1,142 +1,15 @@
 import { exercises } from "@/lib/exercises";
-import Image from "next/image";
-import styled from "styled-components";
-import Link from "next/link";
+import { StyledFlexWrapper, ExerciseList } from "/styledComponents.js";
+import Exercise from "../components/Exercise/Exercise";
 
 export default function HomePage() {
   return (
     <StyledFlexWrapper>
       <ExerciseList>
         {exercises.map((exercise) => (
-          <ExerciseCard key={exercise.id}>
-            <StyledLink href={`/exercises/${exercise.id}`}>
-              <BackgroundImageWrapper>
-                <StyledImage
-                  src={exercise.imageUrl}
-                  alt={exercise.name}
-                  layout="fill"
-                  objectFit="cover"
-                  quality={75}
-                  priority={exercise.id === 0}
-                />
-              </BackgroundImageWrapper>
-              <ContentOverlay>
-                <ExerciseName>{exercise.name}</ExerciseName>
-                <MuscleGroupList>
-                  {exercise.muscleGroups.map((muscle, index) => (
-                    <MuscleBadge key={index}>{muscle}</MuscleBadge>
-                  ))}
-                </MuscleGroupList>
-              </ContentOverlay>
-            </StyledLink>
-          </ExerciseCard>
+          <Exercise key={exercise.id} exercise={exercise} />
         ))}
       </ExerciseList>
     </StyledFlexWrapper>
   );
 }
-
-const StyledFlexWrapper = styled.div`
-  display: flex;
-  justify-content: center;
-`;
-
-const ExerciseList = styled.ul`
-  list-style: none;
-  padding: 0;
-  display: grid;
-  gap: 1.5rem;
-
-  grid-template-columns: repeat(3, 1fr);
-
-  @media (max-width: 1024px) {
-    grid-template-columns: repeat(2, 1fr);
-  }
-
-  @media (max-width: 600px) {
-    grid-template-columns: 1fr;
-  }
-`;
-
-const ExerciseCard = styled.li`
-  position: relative;
-  border-radius: 10px;
-  overflow: hidden;
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-  text-align: center;
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-end;
-  height: 25rem;
-  width: 100%;
-
-  &:hover {
-    background-color: #f0f8ff;
-    transform: scale(1.1);
-  }
-
-  &:active {
-    transform: scale(1.05);
-  }
-`;
-
-const BackgroundImageWrapper = styled.div`
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  z-index: 1;
-`;
-
-const StyledImage = styled(Image)`
-  z-index: 1;
-  border-radius: 10px;
-`;
-
-const ContentOverlay = styled.div`
-  position: relative;
-  z-index: 2;
-  padding: 15px;
-  background: #00000066;
-  color: #fff;
-  width: 100%;
-`;
-
-const ExerciseName = styled.h3`
-  color: #fff;
-  margin: 10px 0;
-`;
-
-const MuscleGroupList = styled.ul`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-  flex-wrap: wrap;
-  gap: 5px;
-  margin: 0;
-  padding: 0;
-  list-style: none;
-`;
-
-const MuscleBadge = styled.li`
-  background-color: #3498db;
-  color: #fff;
-  border-radius: 5px;
-  padding: 5px 10px;
-  font-size: 0.85em;
-  font-weight: bold;
-`;
-
-const StyledLink = styled(Link)`
-  text-decoration: none;
-`;
-
-export {
-  MuscleBadge,
-  MuscleGroupList,
-  BackgroundImageWrapper,
-  StyledImage,
-  ExerciseCard,
-};
--- pages/workoutlist.js
+++ pages/workoutlist.js
@@ -1,10 +1,18 @@
 import { useState } from "react";
-import styled from "styled-components";
 import { exercises } from "@/lib/exercises";
 import { workouts as initialWorkouts } from "@/lib/workouts";
 import Form from "@/components/WorkoutForm";
 import Workout from "@/components/Workout/Workout";
 import { uid } from "uid";
+import {
+  Filter,
+  WorkoutList,
+  StyledHeadline,
+  NoWorkoutsMessage,
+  CreateWorkoutButton,
+  CancelCreateButton,
+  FlexWrapWorkouts,
+} from "@/styledComponents";
 
 function findExerciseById(exerciseId) {
   return exercises.find((exercise) => exercise.id === exerciseId);
@@ -89,91 +97,3 @@ export default function WorkoutsList() {
     </FlexWrapWorkouts>
   );
 }
-
-const Filter = styled.div`
-  width: 100vw;
-  min-height: 100vh;
-  position: fixed;
-  bottom: 0;
-  z-index: 1;
-  background: #00000099;
-  padding: 1rem 3rem;
-`;
-
-const WorkoutList = styled.section`
-  width: 100%;
-  max-width: 1200px;
-  display: flex;
-  justify-content: center;
-  padding: 1rem;
-  flex-direction: column;
-`;
-
-const StyledHeadline = styled.h2`
-  background: #fff;
-  padding: 0.5rem;
-  margin: 0;
-  text-align: center;
-`;
-
-const NoWorkoutsMessage = styled.div`
-  font-size: 1.25rem;
-  color: #3498db;
-  text-align: center;
-  margin: 2rem 0;
-  padding: 1rem 1rem 1.5rem 1rem;
-  background-color: #fefefe;
-  border: 2px solid #3498db;
-  border-radius: 5px;
-`;
-
-const ToggleButton = styled.button`
-  background: #e67e22;
-  color: #fff;
-  border: none;
-  border-radius: 5px;
-  padding: 0.5rem 1rem;
-  cursor: pointer;
-  font-size: 1rem;
-  margin: 1rem 0;
-  display: block;
-  margin-left: auto;
-  margin-right: auto;
-
-  &:hover {
-    background: #d35400;
-  }
-`;
-
-const CreateWorkoutButton = styled(ToggleButton)`
-  background-color: #27ae60;
-  position: fixed;
-  z-index: 5;
-  font-size: 3rem;
-  bottom: 20px;
-  right: 14px;
-  border-radius: 50%;
-  border: 2px solid #fff;
-  width: 80px;
-  height: 80px;
-
-  &:hover {
-    background-color: #1f8a4d;
-  }
-`;
-
-const CancelCreateButton = styled(CreateWorkoutButton)`
-  background-color: #dc3545;
-  transform: rotate(45deg);
-
-  &:hover {
-    background-color: #c82333;
-  }
-`;
-
-const FlexWrapWorkouts = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-`;
