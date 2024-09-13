62d7dcab2726e8525229e0e2e86f866061208b39
Sercan Sebil <sercansebil95@googlemail.com>
2024-09-13 12:12:16 +0200

    font styling

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 import Image from "next/image";
-import { useRouter } from "next/router";
+import Link from "next/link";
 
 const HiddenAppName = styled.h1`
   display: none;
@@ -10,8 +10,8 @@ const StyledHeader = styled.header`
   display: flex;
   justify-content: flex-start;
   align-items: center;
-  padding: 20px;
-  background-color: #ffffff;
+  padding: 10px;
+  background-color: #fff;
   box-shadow:
     0 4px 6px rgba(0, 0, 0, 0.1),
     0 4px 10px -2px rgba(52, 152, 219, 0.7);
@@ -25,58 +25,44 @@ const LogoContainer = styled.div`
 `;
 
 const AppName = styled.h1`
-  font-size: 2.5rem;
+  font-size: 2.2rem;
   color: #3498db;
   margin: 0;
-  font-weight: bold;
+  letter-spacing: 2px;
+  font-weight: 200;
+`;
+
+const StyledLog = styled.span`
+  font-weight: 900;
 `;
 
 const Slogan = styled.p`
   font-size: 1.2rem;
   color: #3498db;
   margin: 0;
-  font-style: italic;
+  font-family: monospace;
 `;
+
 export default function Header() {
-  const router = useRouter();
-  const headerContent = getHeaderContent(router.pathname);
-
-  function getHeaderContent(pathname) {
-    switch (pathname) {
-      case "/exercises/[id]":
-        return (
-          <LogoContainer>
-            <Image
-              src="/Logo_ohne_Schrift.png"
-              alt="Logo"
-              width={90}
-              height={90}
-            />
-            <div>
-              <AppName>Gym Log</AppName>
-              <Slogan>Crush your goals</Slogan>
-            </div>
-            <HiddenAppName>GYM LOG - Crush your Goals</HiddenAppName>
-          </LogoContainer>
-        );
-
-      default:
-        return (
-          <LogoContainer>
-            <Image
-              src="/Logo_ohne_Schrift.png"
-              alt="Logo"
-              width={90}
-              height={90}
-            />
-            <div>
-              <AppName>Gym Log</AppName>
-              <Slogan>Crush your goals</Slogan>
-            </div>
-            <HiddenAppName>GYM LOG - Crush your Goals</HiddenAppName>
-          </LogoContainer>
-        );
-    }
-  }
-  return <StyledHeader>{headerContent}</StyledHeader>;
+  return (
+    <StyledHeader>
+      <LogoContainer>
+        <Link href="/">
+          <Image
+            src="/Logo_ohne_Schrift.png"
+            alt="Logo"
+            width={90}
+            height={90}
+          />
+        </Link>
+        <div>
+          <AppName>
+            GYM<StyledLog>LOG</StyledLog>
+          </AppName>
+          <Slogan>Crush your goals</Slogan>
+        </div>
+        <HiddenAppName>GYM LOG - Crush your Goals</HiddenAppName>
+      </LogoContainer>
+    </StyledHeader>
+  );
 }
