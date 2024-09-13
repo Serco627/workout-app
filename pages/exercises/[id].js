caf56742ab93fcc8cf154194a7c529abd08c4885
Lorenz Krüger <lorenzkrueger@gmail.com>
2024-09-13 12:04:11 +0200

    put muscles in the Spotlight above image

--- pages/exercises/[id].js
+++ pages/exercises/[id].js
@@ -11,6 +11,11 @@ import {
   BackgroundImageWrapper,
   StyledImage,
 } from "/styledComponents";
+import styled from "styled-components";
+
+const ImageWrapper = styled.div`
+  padding: 20px;
+`;
 
 export default function ExerciseDetailsPage() {
   const router = useRouter();
@@ -26,33 +31,28 @@ export default function ExerciseDetailsPage() {
     <Container>
       <StyledExerciseName>{currentExercise.name}</StyledExerciseName>
       <MuscleGroupList>
+        <>
+          <h3>Muscles In The Spotlight</h3>
+        </>
         {currentExercise.muscleGroups.map((muscle, index) => (
           <MuscleBadge key={`${currentExercise.id}-muscle-${index}`}>
             {muscle}
           </MuscleBadge>
         ))}
       </MuscleGroupList>
-
-      <ExerciseDetailsImage>
-        <BackgroundImageWrapper>
-          <StyledImage
-            src={currentExercise.imageUrl}
-            alt={currentExercise.name}
-            layout="fill"
-            objectFit="cover"
-            quality={75}
-          />
-        </BackgroundImageWrapper>
-      </ExerciseDetailsImage>
-      <h3>Muscles In The Spotlight</h3>
-
-      <MuscleGroupList>
-        {currentExercise.muscleGroups.map((muscle, index) => (
-          <MuscleBadge key={`${currentExercise.id}-muscle-${index}`}>
-            {muscle}
-          </MuscleBadge>
-        ))}
-      </MuscleGroupList>
+      <ImageWrapper>
+        <ExerciseDetailsImage>
+          <BackgroundImageWrapper>
+            <StyledImage
+              src={currentExercise.imageUrl}
+              alt={currentExercise.name}
+              layout="fill"
+              objectFit="cover"
+              quality={75}
+            />
+          </BackgroundImageWrapper>
+        </ExerciseDetailsImage>
+      </ImageWrapper>
 
       <InstructionsList>
         <h3>How To Crush It</h3>
